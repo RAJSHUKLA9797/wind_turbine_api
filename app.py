6971e9e4784d96ebf777bacea68ee6b7bc98db62
Raj Shukla <101661663+RAJSHUKLA9797@users.noreply.github.com>
2024-11-17 23:44:30 +0530

    Update app.py11

--- app.py
+++ app.py
@@ -396,4 +396,4 @@ def calculate_fzb():
         return render_template('results.html', error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=10000)
+    app.run(host='0.0.0.0', port=10000)
